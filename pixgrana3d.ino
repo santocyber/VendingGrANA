00e28672efa90ea77d862f4fafd119f5a5fd9645
SantoCyber <santocyber@gmail.com>
2023-08-30 19:09:46 -0300

    Create pixgrana3d.ino

---
+++ pixgrana3d.ino
@@ -0,0 +1,256 @@
+
+#include <WiFi.h>
+#include "Arduino.h"
+#include <WiFiMulti.h>
+
+#include <HTTPClient.h>
+
+const char* ssid = "InternetSA";
+const char* password = "cadebabaca";
+const char* host = "santocyber.helioho.st";
+String url = "http://santocyber.helioho.st/pix-gateway/v1/api_qrcode.php";
+String url2 = "http://santocyber.helioho.st/pix-gateway/v1/api_orders.php";
+String url3 = "http://santocyber.helioho.st/pix-gateway/v1/api_webhook.php";
+
+const int httpPort = 80;
+
+
+const int ledPin = 48;     // Pino do LED
+
+// Variáveis para rastrear o estado do botão e os cliques
+int buttonState = 0;       // Variável para armazenar o estado do botão
+
+int Button1 = 0;  // Pino do botão
+
+int contacendled = 0;
+
+unsigned long intervalo = 0;
+
+// Setting clock just to be sure...
+void setClock() {
+  configTime(0, 0, "pool.ntp.org");
+
+  Serial.print(F("Waiting for NTP time sync: "));
+  time_t nowSecs = time(nullptr);
+  while (nowSecs < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(F("."));
+    yield();
+    nowSecs = time(nullptr);
+  }
+
+  Serial.println();
+  struct tm timeinfo;
+  gmtime_r(&nowSecs, &timeinfo);
+  Serial.print(F("Current time: "));
+  Serial.print(asctime(&timeinfo));
+}
+
+
+WiFiMulti WiFiMulti;
+
+void setup() {
+
+
+
+  pinMode(Button1, INPUT); // Define o pino do botão como entrada
+  pinMode(ledPin, OUTPUT);   // Define o pino do LED como saída
+
+  
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("InternetSA", "cadebabaca");
+
+  // wait for WiFi connection
+  Serial.print("Waiting for WiFi to connect...");
+  while ((WiFiMulti.run() != WL_CONNECTED)) {
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  setClock();  
+}
+
+
+void loop() {
+
+
+    buttonState = digitalRead(Button1); // Lê o estado do botão
+
+ if (buttonState == LOW) { // Se o botão estiver pressionado
+ //if ( digitalRead(Button1)==1) {
+      Serial.println("botao pressionado");
+      contacendled++;
+      delay(500);       
+      intervalo = millis();}
+
+// se passar mais que 2 segundos entre os pressionar de botões, ele volta a zero e precisa pressionar duas vezes de novo      
+  if(millis() > intervalo+2000){
+  contacendled = 0;
+}    
+ 
+ //  se a variavel contacendled chegar a 2 o que dizer que o botão 1 foi pressionado 2 vezes ele acende o led 1 
+  if(contacendled == 2){
+      neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0); // Green
+            Serial.println("botao 2");
+
+
+  }
+  
+  //  se a variavel contacendled chegar a 3 o que dizer que o botão 1 foi pressionado 2 vezes ele acende o led 1 
+  if(contacendled == 3){
+  neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS); // Blue
+        Serial.println("botao 3");
+
+  }
+
+
+
+   //  se a variavel contacendled chegar a 2 o que dizer que o botão 1 foi pressionado 2 vezes ele acende o led 1 
+  if(contacendled == 1){
+       neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0); // Red
+
+            Serial.println("botao 1");
+            pix();
+
+
+  }
+
+
+      Serial.println(contacendled);
+
+}
+  
+
+  void pix(){
+WiFiClient client;
+
+
+  if (client.connect(host, 80)) 
+  {
+  Serial.println(F("connected"));
+  
+  String CHAVEPHP = "?id=";
+         CHAVEPHP += "QRCODE";
+
+
+
+  client.print(F("GET ")); client.print(url); client.print(CHAVEPHP);  
+  client.println(F(" HTTP/1.0")); 
+  client.println(F("Host: SomeHost"));
+  client.println();
+    neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0); // Green
+
+
+  } 
+   else 
+    {
+    Serial.println(F("connection failed")); Serial.println();
+      neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0); // Red
+    }
+
+  while(client.connected() && !client.available()) delay(1); //waits for data
+  while (client.connected() || client.available()) 
+  { //connected or data available
+    char c = client.read();
+    Serial.print(c);
+    
+  }
+  neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS); // Blue
+  delay(1000);
+ 
+  Serial.println("");
+  Serial.println(F("Desconectando."));
+  client.stop();
+  digitalWrite(RGB_BUILTIN, LOW);
+}
+
+      void orders(){
+WiFiClient client;
+
+
+  if (client.connect(host, 80)) 
+  {
+  Serial.println(F("connected"));
+  
+
+  client.print(F("GET ")); client.print(url2);  
+  client.println(F(" HTTP/1.0")); 
+  client.println(F("Host: SomeHost"));
+  client.println();
+    neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0); // Green
+
+
+  } 
+   else 
+    {
+    Serial.println(F("connection failed")); Serial.println();
+      neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0); // Red
+    }
+
+  while(client.connected() && !client.available()) delay(1); //waits for data
+  while (client.connected() || client.available()) 
+  { //connected or data available
+    char c = client.read();
+    Serial.print(c);
+    
+  }
+  neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS); // Blue
+  delay(1000);
+ 
+  Serial.println("");
+  Serial.println(F("Desconectando."));
+  client.stop();
+  digitalWrite(RGB_BUILTIN, LOW);
+}
+    
+    
+     void webhook(){
+WiFiClient client;
+
+
+  if (client.connect(host, 80)) 
+  {
+  Serial.println(F("connected"));
+  
+
+  client.print(F("GET ")); client.print(url3);  
+  client.println(F(" HTTP/1.0")); 
+  client.println(F("Host: SomeHost"));
+  client.println();
+    neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0); // Green
+
+
+  } 
+   else 
+    {
+    Serial.println(F("connection failed")); Serial.println();
+      neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0); // Red
+    }
+
+  while(client.connected() && !client.available()) delay(1); //waits for data
+  while (client.connected() || client.available()) 
+  { //connected or data available
+    char c = client.read();
+    Serial.print(c);
+    
+  }
+  neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS); // Blue
+  delay(1000);
+ 
+  Serial.println("");
+  Serial.println(F("Desconectando."));
+  client.stop();
+  digitalWrite(RGB_BUILTIN, LOW);
+}
+    
+    
+    
